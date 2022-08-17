3904230ea338c6ca12ee5fd865dbb0a8f72fbb2b
bawrdiii <bardiabarbod@live.com>
2022-08-17 13:48:23 +0200

    .

--- src/components/badges.jsx
+++ src/components/badges.jsx
@@ -1,2 +1,11 @@
 
-const 
+const Badges = ({ src, skill, className }) => {
+    return (
+        <figure className={`badge ${className}`}>
+            <img src={src} alt={skill} />
+            <figcaption>{skill}</figcaption>
+        </figure>
+    )
+}
+
+export default Badges
--- src/components/english/second.jsx
+++ src/components/english/second.jsx
@@ -1,3 +1,4 @@
+import Badges from "../badges"
 
 
 const Second = ({ lang }) => {
@@ -7,15 +8,46 @@ const Second = ({ lang }) => {
 
 
         <section className="skill-section p-1">
-            <h2 className="title">
+            <h2 className="title text-center mb-1">
                 {!lang ? `My knowledges` : `Meine Kenntnisse`}
             </h2>
             <section className="skills d-grid grid-skills mt-1">
-                <figure className="badge">
-                    <img src="/icons/javascript.svg" alt="javascript" />
-                    <figcaption>JavaScript</figcaption>
-                </figure>
-
+                <Badges src="icons/html5.svg"
+                    skill='HTML5' />
+                <Badges src='icons/css3.svg'
+                    skill='CSS3'
+                />
+                <Badges
+                    skill='Bootstrap'
+                    src='icons/bootstrap.svg'
+                />
+                <Badges
+                    src="icons/javascript.svg"
+                    skill="Javascript"
+                />
+                <Badges
+                    skill='React.js'
+                    src='icons/reactjs-icon.svg'
+                />
+                <Badges src='icons/next-js-logo-7929BCD36F-seeklogo.com.png'
+                    skill='Next.js'
+                    className='nextjs'
+                />
+                <Badges
+                    skill='Git'
+                    src='icons/git.svg'
+                />
+                <Badges
+                    skill="Word"
+                    src='icons/icons8-microsoft-word-2019-50.svg'
+                />
+                <Badges skill='Power Point'
+                    src='icons/icons8-microsoft-powerpoint-48.svg'
+                />
+                <Badges
+                    skill='Visual studio'
+                    src='icons/icons8-visual-studio-code-2019-50.svg'
+                />
             </section>
 
         </section>
